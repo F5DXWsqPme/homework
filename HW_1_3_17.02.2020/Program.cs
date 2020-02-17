695c3c917119be37f70059da86990d7e11f34dfa
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-02-17 18:21:34 +0300

    task 1.3 added

---
+++ HW_1_3_17.02.2020/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace SomeNamespace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int length = 10;
+            int[] array = new int[length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array.Length - i;
+                System.Console.Write($"{array[i]} ");
+            }
+
+            System.Console.Write("\n");
+
+            SelectionSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                System.Console.Write($"{array[i]} ");
+            }
+
+            System.Console.Write("\n");
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        private static void SelectionSort(int[] array)
+        {
+            for (int j = 0; j < array.Length - 1; j++)
+            {
+                int min = j;
+                for (int i = j + 1; i < array.Length; i++)
+                {
+                    if (array[i] < array[min])
+                    {
+                        min = i;
+                    }
+                }
+
+                Swap(ref array[j], ref array[min]);
+            }
+        }
+    }
+}
