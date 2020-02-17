b73a56630215ab5ce843d123b889610ebf183fda
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-02-17 18:17:35 +0300

    task 1.2 added

---
+++ HW_1_2_17.02.2020/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SomeNamespace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine($"Fibonacci(10) = {Fibonacci(10)}");
+        }
+
+        private static int Fibonacci(int num)
+        {
+            int fib0 = 0, fib1 = 1, newFib;
+
+            for (int i = 0; i < num; i++)
+            {
+                newFib = fib0 + fib1;
+                fib0 = fib1;
+                fib1 = newFib;
+            }
+
+            return fib0;
+        }
+    }
+}
