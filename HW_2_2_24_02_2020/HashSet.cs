a10b63385ba6644f878a6df40d1b75b644919345
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-02-24 15:47:05 +0300

    task 2.2 added

---
+++ HW_2_2_24_02_2020/HashSet.cs
@@ -0,0 +1,54 @@
+﻿namespace HW_2_2_24_02_2020
+{
+    internal class HashSet
+    {
+        private const int ArraySize = 1 << 12;
+        private List[] array;
+
+        public HashSet()
+        {
+            this.array = new List[ArraySize];
+
+            for (int i = 0; i < ArraySize; i++)
+            {
+                this.array[i] = new List();
+            }
+        }
+
+        public void AddElement(int value)
+        {
+            int hash = this.EvalHash(value);
+            List list = this.array[hash];
+
+            if (!list.IsItemExists(value))
+            {
+                list.AddElement(value, 0);
+            }
+        }
+
+        public bool IsElementExists(int value)
+        {
+            int hash = this.EvalHash(value);
+            List list = this.array[hash];
+
+            return list.IsItemExists(value);
+        }
+
+        public void DeleteElement(int value)
+        {
+            int hash = this.EvalHash(value);
+            List list = this.array[hash];
+
+            int position = 0;
+            if (list.GetElementPosition(value, ref position))
+            {
+                list.DeleteElement(position);
+            }
+        }
+
+        private int EvalHash(int number)
+        {
+            return number % ArraySize;
+        }
+    }
+}
