5c26e1f409edc79e855b9f9bece234a0d5c9a7ff
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-02-24 20:58:15 +0300

    Calculator is working now

---
+++ HW_2_3_24_02_2020/Evaluator.cs
@@ -0,0 +1,28 @@
+﻿namespace HW_2_3_24_02_2020
+{
+    internal class Evaluator
+    {
+        public double Evaluate(IQueue tokens)
+        {
+            IStack stack = (IStack)new StackArray();
+
+            while (!tokens.IsEmpty())
+            {
+                IToken current = tokens.Get();
+
+                if (current is Number number)
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    Number right = (Number)stack.Pop();
+                    Number left = (Number)stack.Pop();
+                    stack.Push(((Operator)current).Evaluate(left, right));
+                }
+            }
+
+            return ((Number)stack.Pop()).Get();
+        }
+    }
+}
