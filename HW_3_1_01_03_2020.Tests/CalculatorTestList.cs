917cd4698f27ac9e1ee3f081e38802f6111ed9e0
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-03-01 19:59:35 +0300

    Task 3.1 added

---
+++ HW_3_1_01_03_2020.Tests/CalculatorTestList.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace HW_3_1_01_03_2020.Test
+{
+    public class CalculatorTestList
+    {
+        Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator(new StackList());
+        }
+
+        [Test]
+        public void CalculatorShouldThrowExceptionWhenArgumentEmpty()
+        {
+            Assert.Throws<System.ArgumentException>(() => calculator.Evaluate(string.Empty));
+            Assert.Throws<System.ArgumentException>(() => calculator.Evaluate("   "));
+        }
+
+        [Test]
+        public void CalculatorShouldThrowExceptionWhenArgumentIncorrectForEvaluator()
+        {
+            Assert.Throws<System.ArgumentException>(() => calculator.Evaluate("1 2 -   +"));
+        }
+
+        [Test]
+        public void CalculatorShouldThrowExceptionWhenArgumentIncorrectForScanner()
+        {
+            Assert.Throws<System.ArgumentException>(() => calculator.Evaluate("1,2,3"));
+            Assert.Throws<System.ArgumentException>(() => calculator.Evaluate("  &"));
+        }
+
+        [Test]
+        public void CalculatorShouldEvaluateExpression()
+        {
+            double result = calculator.Evaluate("1 2  3+- 4*8/");
+
+            Assert.AreEqual(-2, result);
+        }
+
+        [Test]
+        public void CalculatorShouldReturnsNumber()
+        {
+            double result = calculator.Evaluate("5");
+
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ HW_3_1_01_03_2020.Tests/EvaluatorTestArray.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace HW_3_1_01_03_2020.Test
+{
+    public class EvaluatorTestArray
+    {
+        Evaluator evaluator;
+
+        [SetUp]
+        public void Setup()
+        {
+            evaluator = new Evaluator(new StackArray());
+        }
+
+        [Test]
+        public void EvaluatorShouldThrowExceptionWhenArgumentEmpty()
+        {
+            Assert.Throws<System.ArgumentException>(() => evaluator.Evaluate(new QueueArray()));
+        }
+
+        [Test]
+        public void EvaluatorShouldEvaluateExpression()
+        {
+            var tokens = new QueueArray();
+
+            tokens.Put(new Number(1));
+            tokens.Put(new Number(2));
+            tokens.Put(new Number(3));
+            tokens.Put(new Operator('+'));
+            tokens.Put(new Operator('-'));
+            tokens.Put(new Number(4));
+            tokens.Put(new Operator('*'));
+            tokens.Put(new Number(8));
+            tokens.Put(new Operator('/'));
+
+            double result = evaluator.Evaluate(tokens);
+
+            Assert.AreEqual(-2, result);
+        }
+
+        [Test]
+        public void EvaluatorShouldThrowExceptionWhenArgumentsIncorrect()
+        {
+            var tokens = new QueueArray();
+
+            tokens.Put(new Number(1));
+            tokens.Put(new Number(2));
+            tokens.Put(new Operator('+'));
+            tokens.Put(new Operator('-'));
+
+            Assert.Throws<System.ArgumentException>(() => evaluator.Evaluate(tokens));
+        }
+    }
+}
