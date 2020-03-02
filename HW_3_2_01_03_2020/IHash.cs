b055cdf53f68940984198b69a91a38141efdaf05
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-03-02 16:43:36 +0300

    task 3.2 added

---
+++ HW_3_2_01_03_2020/IHash.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// Global namespace.
+/// </summary>
+namespace HW_3_2_01_03_2020
+{
+    /// <summary>
+    /// Hash interface.
+    /// </summary>
+    public interface IHash
+    {
+        /// <summary>
+        /// Gets array size in hash table.
+        /// </summary>
+        /// <returns>Array size (maximum hash + 1).</returns>
+        public abstract int GetArraySize();
+
+        /// <summary>
+        /// Gets element hash.
+        /// </summary>
+        /// <returns>Element hash.</returns>
+        public abstract int GetHash(int element);
+    }
+}
