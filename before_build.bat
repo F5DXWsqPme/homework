52d8b83ca96b12a2d4967b6f86fd7b304219aac7
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-04-15 13:43:37 +0300

    Appveyor changes

--- before_build.bat
+++ before_build.bat
@@ -1,7 +1,7 @@
 FOR /D %%n IN (HW2t_*) DO ( 
-    nuget restore %%n/Solution/Solution.csproj
+    nuget restore %%n/Solution/Solution.sln
 )
 
 FOR /D %%n IN (HW2wt_*) DO ( 
-    nuget restore %%n/Solution/Solution.csproj
+    nuget restore %%n/Solution/Solution.sln
 )
--- build_script.bat
+++ build_script.bat
@@ -1,7 +1,7 @@
 FOR /D %%n IN (HW2t_*) DO ( 
-    dotnet build %%n/Solution/Solution.csproj
+    dotnet build %%n/Solution/Solution.sln
 )
 
 FOR /D %%n IN (HW2wt_*) DO ( 
-    dotnet build %%n/Solution/Solution.csproj
+    dotnet build %%n/Solution/Solution.sln
 )
