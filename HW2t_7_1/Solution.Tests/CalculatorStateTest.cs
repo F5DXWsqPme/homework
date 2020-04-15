bc98da028fec3591f9f6b35db4f44420b590c366
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-04-15 22:20:34 +0300

    Version with tests

--- HW2t_7_1/Solution.Tests/CalculatorStateTest.cs
+++ HW2t_7_1/Solution.Tests/CalculatorStateTest.cs
@@ -10,9 +10,314 @@ namespace Solution.Tests
         }
 
         [Test]
-        public void CalculatorStateShouldPassTest()
+        public void CalculatorShouldScan()
         {
-            Assert.Pass();
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update("2");
+            state.Update("3");
+
+            Assert.AreEqual("+123", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldScanZero()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("0");
+            state.Update("0");
+            state.Update("1");
+            state.Update("2");
+            state.Update("0");
+            state.Update("3");
+
+            Assert.AreEqual("+1203", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldGetRightSubstraction()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update("2");
+            state.Update("-");
+            state.Update("5");
+            state.Update("2");
+            state.Update("=");
+
+            Assert.AreEqual("-40", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldGetRightAddition()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update("2");
+            state.Update("+");
+            state.Update("5");
+            state.Update("2");
+            state.Update("=");
+
+            Assert.AreEqual("+64", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldGetRightMultiplication()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update("2");
+            state.Update("*");
+            state.Update("5");
+            state.Update("=");
+
+            Assert.AreEqual("+60", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldGetRightDivision()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update("2");
+            state.Update("/");
+            state.Update("4");
+            state.Update("=");
+
+            Assert.AreEqual("+3", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldGetRightDivisionFloat()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update("2");
+            state.Update("/");
+            state.Update("7");
+            state.Update(".");
+            state.Update("1");
+            state.Update("=");
+
+            Assert.AreEqual("+1,69", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldChangeSign()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update("2");
+            state.Update("3");
+            state.Update("+/-");
+
+            Assert.AreEqual("-123", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldChangeSignInNewNumber()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update("2");
+            state.Update("3");
+            state.Update("+");
+            state.Update("7");
+            state.Update("+/-");
+            state.Update("7");
+            state.Update("=");
+
+            Assert.AreEqual("+46", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldRightRound()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("2");
+            state.Update("/");
+            state.Update("3");
+            state.Update("=");
+
+            Assert.AreEqual("+0,67", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldWorkAfterClearLikeNew()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("3");
+            state.Update("+");
+            state.Update("2");
+            state.Update("C");
+            state.Update("6");
+            state.Update("+");
+            state.Update("7");
+            state.Update("=");
+
+            Assert.AreEqual("+13", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldWorkAfterEqualWithResult()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("3");
+            state.Update("+");
+            state.Update("2");
+            state.Update("=");
+            state.Update("-");
+            state.Update("7");
+            state.Update(".");
+            state.Update("5");
+            state.Update("=");
+
+            Assert.AreEqual("-2,5", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldWorkAfterEqualWithoutResult()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("3");
+            state.Update("+");
+            state.Update("2");
+            state.Update("=");
+            state.Update("9");
+            state.Update("-");
+            state.Update("7");
+            state.Update(".");
+            state.Update("5");
+            state.Update("=");
+
+            Assert.AreEqual("+1,5", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldRightWorkWithBigDisplaySize1()
+        {
+            var state = new Solution.CalculatorState(12);
+
+            state.Update("9");
+            state.Update("-");
+            state.Update("7");
+            state.Update(".");
+            state.Update("5");
+            state.Update("=");
+
+            Assert.AreEqual("+1,5", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldRightWorkWithBigDisplaySize2()
+        {
+            var state = new Solution.CalculatorState(12);
+
+            state.Update("2");
+            state.Update("/");
+            state.Update("3");
+            state.Update("=");
+
+            Assert.AreEqual("+0,666666667", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldRightWorkWithLowDisplaySize()
+        {
+            var state = new Solution.CalculatorState(2);
+
+            state.Update("2");
+            state.Update("-");
+            state.Update("3");
+            state.Update("=");
+
+            Assert.AreEqual("-1", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldThrowWhenWrongDigit()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            Assert.Throws<System.ArgumentException>(() => state.Update("10.0"));
+            Assert.Throws<System.ArgumentException>(() => state.Update("-1"));
+            Assert.Throws<System.ArgumentException>(() => state.Update("5."));
+        }
+
+        [Test]
+        public void CalculatorShouldThrowWhenUnknownSymbol()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            Assert.Throws<System.ArgumentException>(() => state.Update("?"));
+            Assert.Throws<System.ArgumentException>(() => state.Update("^"));
+            Assert.Throws<System.ArgumentException>(() => state.Update(":"));
+        }
+
+        [Test]
+        public void CalculatorShouldThrowWhenDisplaySizeLessThan2()
+        {
+            Assert.Throws<System.ArgumentException>(() => new Solution.CalculatorState(1));
+        }
+
+        [Test]
+        public void CalculatorShouldGetErrorWhenNumberVeryBig1()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update("2");
+            state.Update("3");
+            state.Update("4");
+            state.Update("5");
+
+            Assert.AreEqual("ERROR", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldGetErrorWhenNumberVeryBig2()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update("2");
+            state.Update("3");
+            state.Update("4");
+            state.Update("+");
+            state.Update("9");
+            state.Update("9");
+            state.Update("9");
+            state.Update("9");
+            state.Update("=");
+
+            Assert.AreEqual("ERROR", state.GetOutputString());
+        }
+
+        [Test]
+        public void CalculatorShouldGetErrorWhenInNumber2Dot()
+        {
+            var state = new Solution.CalculatorState(5);
+
+            state.Update("1");
+            state.Update(".");
+            state.Update("1");
+            state.Update(".");
+
+            Assert.AreEqual("ERROR", state.GetOutputString());
         }
     }
 }
--- HW2t_7_1/Solution/CalculatorState.cs
+++ HW2t_7_1/Solution/CalculatorState.cs
@@ -27,8 +27,15 @@ namespace Solution
         /// Initializes a new instance of the <see cref="CalculatorState"/> class.
         /// </summary>
         /// <param name="displaySize">Output string length.</param>
+        /// <exception cref="ArgumentException">Throws when display size less than 2.</exception>
         public CalculatorState(int displaySize)
         {
+            int minimalDisplaySize = 2;
+            if (displaySize < minimalDisplaySize)
+            {
+                throw new ArgumentException("Wrong dispaly size");
+            }
+
             this.resultNumber = 0;
             this.newNumber = 0;
             this.errorFlag = false;
@@ -55,17 +62,23 @@ namespace Solution
                 return errorString.Substring(0, Math.Min(this.displaySize, errorString.Length));
             }
 
-            string format = "G";
-            string valueString;
+            decimal value;
 
             if (this.operatorString == string.Empty)
             {
-                valueString = this.resultNumber.ToString(format);
+                value = this.resultNumber;
             }
             else
             {
-                valueString = this.newNumber.ToString(format);
+                value = this.newNumber;
             }
+
+            int numberOfIntegerDigits = (int)Math.Max(Math.Ceiling(Math.Log10(Math.Abs((double)value))), 1);
+            int precision = Math.Min(Math.Max(this.displaySize - numberOfIntegerDigits - 2, 0), 28);
+
+            value = Math.Round(value, precision, MidpointRounding.AwayFromZero);
+
+            string valueString = value.ToString("G");
 
             if (!valueString.StartsWith("-"))
             {
@@ -89,37 +102,7 @@ namespace Solution
         {
             if (int.TryParse(clickedButtonText, out int digit))
             {
-                if (this.equalFlag)
-                {
-                    this.resultNumber = 0;
-                    this.equalFlag = false;
-                }
-
-                if (digit > 9 || digit < 0)
-                {
-                    throw new ArgumentException("Unknown button");
-                }
-
-                Func<decimal, decimal> transform;
-
-                if (this.dotPosition == 0)
-                {
-                    transform = (decimal number) => (10 * number) + (this.sign * digit);
-                }
-                else
-                {
-                    this.dotPosition++;
-                    transform = (decimal number) => number + (this.sign * digit * (decimal)Math.Pow(10, 1 - this.dotPosition));
-                }
-
-                if (this.operatorString == string.Empty)
-                {
-                    this.resultNumber = transform(this.resultNumber);
-                }
-                else
-                {
-                    this.newNumber = transform(this.newNumber);
-                }
+                this.UpdateDigit(digit, clickedButtonText);
             }
             else
             {
@@ -159,43 +142,83 @@ namespace Solution
                 }
                 else
                 {
-                    if (this.operatorString != string.Empty)
-                    {
-                        switch (this.operatorString)
-                        {
-                            case "+":
-                                this.resultNumber += this.newNumber;
-                                break;
-                            case "-":
-                                this.resultNumber -= this.newNumber;
-                                break;
-                            case "*":
-                                this.resultNumber *= this.newNumber;
-                                break;
-                            case "/":
-                                this.resultNumber /= this.newNumber;
-                                break;
-                        }
-                    }
-
-                    this.newNumber = 0;
-                    this.dotPosition = 0;
-                    this.operatorString = clickedButtonText;
-                    this.sign = 1;
-
-                    if (this.operatorString == "=")
-                    {
-                        this.operatorString = string.Empty;
-                        this.equalFlag = true;
-                    }
-                    else if (this.operatorString != "+" &&
-                             this.operatorString != "-" &&
-                             this.operatorString != "*" &&
-                             this.operatorString != "/")
-                    {
-                        throw new ArgumentException("Unknown button");
-                    }
+                    this.UpdateOperator(clickedButtonText);
                 }
+            }
+        }
+
+        private void UpdateDigit(int digit, string clickedButtonText)
+        {
+            if (this.equalFlag)
+            {
+                this.resultNumber = 0;
+                this.equalFlag = false;
+            }
+
+            if (clickedButtonText.Length != 1)
+            {
+                throw new ArgumentException("Unknown button");
+            }
+
+            Func<decimal, decimal> transform;
+
+            if (this.dotPosition == 0)
+            {
+                transform = (decimal number) => (10 * number) + (this.sign * digit);
+            }
+            else
+            {
+                this.dotPosition++;
+                transform = (decimal number) => number + (this.sign * digit * (decimal)Math.Pow(10, 1 - this.dotPosition));
+            }
+
+            if (this.operatorString == string.Empty)
+            {
+                this.resultNumber = transform(this.resultNumber);
+            }
+            else
+            {
+                this.newNumber = transform(this.newNumber);
+            }
+        }
+
+        private void UpdateOperator(string clickedButtonText)
+        {
+            if (this.operatorString != string.Empty)
+            {
+                switch (this.operatorString)
+                {
+                    case "+":
+                        this.resultNumber += this.newNumber;
+                        break;
+                    case "-":
+                        this.resultNumber -= this.newNumber;
+                        break;
+                    case "*":
+                        this.resultNumber *= this.newNumber;
+                        break;
+                    case "/":
+                        this.resultNumber /= this.newNumber;
+                        break;
+                }
+            }
+
+            this.newNumber = 0;
+            this.dotPosition = 0;
+            this.operatorString = clickedButtonText;
+            this.sign = 1;
+
+            if (this.operatorString == "=")
+            {
+                this.operatorString = string.Empty;
+                this.equalFlag = true;
+            }
+            else if (this.operatorString != "+" &&
+                     this.operatorString != "-" &&
+                     this.operatorString != "*" &&
+                     this.operatorString != "/")
+            {
+                throw new ArgumentException("Unknown button");
             }
         }
     }
