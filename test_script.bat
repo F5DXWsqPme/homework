4c0299f17c07ab1a34cb71e0980c92bec67fd5b0
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-04-15 22:27:13 +0300

    Appveyor changes

--- test_script.bat
+++ test_script.bat
@@ -1,7 +1,7 @@
 FOR /D %%n IN (HW2t_*) DO ( 
-    dotnet test %%n/Solution/Solution.sln
+    mstest %%n/Solution/Solution.sln
 )
 
 FOR /D %%n IN (HW2wt_*) DO ( 
-    dotnet test %%n/Solution/Solution.sln
+    mstest %%n/Solution/Solution.sln
 )
