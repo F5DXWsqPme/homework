67ae328145d82491e831dc58c3879629d079c631
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-04-16 15:55:23 +0300

    appveyor changes

--- test_script.bat
+++ test_script.bat
@@ -4,9 +4,8 @@ FOR /D %%n IN (HW2t_*) DO (
 
 FOR /D %%n IN (HW2ft_*) DO ( 
     IF EXIST %%n/Solution.Tests\bin\Debug\netcoreapp3.1\Solution.Tests.dll (
-        clr net-4.7 nunit-console %%n/Solution.Tests\bin\Debug\netcoreapp3.1\Solution.Tests.dll
-    )
-    ELSE (
-        clr net-4.7 nunit-console %%n/Solution.Tests\bin\Debug\netcoreapp3.1\Solution.Tests.dll
+        clr.bat net-4.7 nunit-console %%n/Solution.Tests\bin\Debug\netcoreapp3.1\Solution.Tests.dll
+    ) ELSE (
+        clr.bat net-4.7 nunit-console %%n/Solution.Tests\bin\Debug\netcoreapp3.1\Solution.Tests.dll
     )
 )
