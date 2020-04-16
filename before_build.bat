3f0f7aec8623e0da82b0184af34b65f075a6b482
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-04-16 15:08:45 +0300

    appveyor changes

--- before_build.bat
+++ before_build.bat
@@ -5,3 +5,7 @@ FOR /D %%n IN (HW2t_*) DO (
 FOR /D %%n IN (HW2wt_*) DO ( 
     nuget restore %%n/Solution/Solution.sln
 )
+
+FOR /D %%n IN (HW2ft_*) DO ( 
+    nuget restore %%n/Solution/Solution.sln
+)
