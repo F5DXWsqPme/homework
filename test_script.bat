ab6d5fe70a5465da654a1c906c4a3e2284f82ddd
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-04-16 15:06:05 +0300

    appveyor changes

--- test_script.bat
+++ test_script.bat
@@ -2,6 +2,6 @@ FOR /D %%n IN (HW2t_*) DO (
     dotnet test %%n/Solution/Solution.sln
 )
 
-FOR /D %%n IN (HW2wt_*) DO ( 
-    dotnet test %%n/Solution/Solution.sln
+FOR /D %%n IN (HW2ft_*) DO ( 
+    nunit-console %%n/Solution.Tests/Solution.Tests.csproj
 )
