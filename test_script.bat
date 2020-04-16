3dabccd105c6db0a276daf30111aec999a9020f8
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-04-16 15:37:36 +0300

    appveyor changes

--- test_script.bat
+++ test_script.bat
@@ -3,5 +3,6 @@ FOR /D %%n IN (HW2t_*) DO (
 )
 
 FOR /D %%n IN (HW2ft_*) DO ( 
-    nunit-console %%n/Solution/Solution.sln
+    nunit-console %%n/Solution.Tests\bin\Debug\netcoreapp3.1\Solution.Tests.dll
+    nunit-console %%n/Solution.Tests\bin\Release\netcoreapp3.1\Solution.Tests.dll
 )
