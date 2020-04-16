9f88cf509a919b924683e9e13e35cec899f1017a
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-04-16 13:47:13 +0300

    appveyor changes

--- build_script.bat
+++ build_script.bat
@@ -1,7 +1,7 @@
 FOR /D %%n IN (HW2t_*) DO ( 
-    msbuild %%n/Solution/Solution.sln
+    dotnet build %%n/Solution/Solution.sln
 )
 
 FOR /D %%n IN (HW2wt_*) DO ( 
-    msbuild %%n/Solution/Solution.sln
+    dotnet build %%n/Solution/Solution.sln
 )
