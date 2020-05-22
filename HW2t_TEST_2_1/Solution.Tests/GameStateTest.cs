cc6e1fa5f8f2ab0eb065de7e9aff2d9626e14a2c
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-05-22 18:44:34 +0300

    sec rev

--- HW2t_TEST_2_1/Solution.Tests/GameStateTest.cs
+++ HW2t_TEST_2_1/Solution.Tests/GameStateTest.cs
@@ -4,9 +4,12 @@ namespace Solution.Tests
 {
     public class CalculatorStateTest
     {
+        private GameState state;
+
         [SetUp]
         public void Setup()
         {
+            state = new GameState(4);
         }
 
         [Test]
@@ -23,6 +26,40 @@ namespace Solution.Tests
             Assert.Throws<System.ArgumentException>(() => new Solution.GameState(1));
             Assert.Throws<System.ArgumentException>(() => new Solution.GameState(3));
             Assert.Throws<System.ArgumentException>(() => new Solution.GameState(5));
+        }
+
+        [Test]
+        public void GameShouldPressButton()
+        {
+            var changes = state.Update(0, out bool isWin);
+
+            Assert.AreEqual(changes.Length, 1);
+            Assert.AreEqual(changes[0].cellIndex, 0);
+            Assert.AreEqual(changes[0].changedCell.State, GameState.CellState.Pressed);
+            Assert.AreEqual(isWin, false);
+        }
+
+        [Test]
+        public void GameShouldPressButton2()
+        {
+            state.Update(0, out bool isWin0);
+            var changes = state.Update(1, out bool isWin1);
+
+            Assert.AreEqual(changes.Length, 1);
+            Assert.AreEqual(changes[0].cellIndex, 1);
+            Assert.AreEqual(changes[0].changedCell.State, GameState.CellState.Pressed);
+            Assert.AreEqual(isWin1, false);
+        }
+
+        [Test]
+        public void GameShouldPressButton3()
+        {
+            state.Update(0, out bool isWin0);
+            state.Update(1, out bool isWin1);
+            var changes = state.Update(2, out bool isWin2);
+
+            Assert.AreEqual(changes.Length, 3);
+            Assert.AreEqual(isWin2, false);
         }
     }
 }
