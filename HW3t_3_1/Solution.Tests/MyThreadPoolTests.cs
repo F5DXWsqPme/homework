27832ed93b70fec1b96868f07089a4c8a007e9c4
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-09-21 09:41:27 +0300

    Small fixes

--- HW3t_3_1/Solution.Tests/MyThreadPoolTests.cs
+++ HW3t_3_1/Solution.Tests/MyThreadPoolTests.cs
@@ -1,10 +1,10 @@
 namespace Solution.Tests
 {
+    using System;
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.Threading;
     using NUnit.Framework;
-    using System;
 
     public class MyThreadPoolTests
     {
