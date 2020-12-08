fd8da6814826436f9fe30268d0264314340a68b8
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-12-08 11:14:45 +0300

    tests

--- HW3t_TEST2/Solution.Tests/HasherTest.cs
+++ HW3t_TEST2/Solution.Tests/HasherTest.cs
@@ -33,5 +33,45 @@ namespace Solution.Tests
             Assert.IsTrue(success);
             Assert.AreEqual(128 / 8, hash.ToArray().Length);
         }
+
+        [Test]
+        public void HasherShouldGetEqualHash()
+        {
+            var testDir1 = "TestDir/HasherShouldGetDifferentHash1";
+            var testDir2 = "TestDir/HasherShouldGetDifferentHash2";
+
+            Directory.CreateDirectory(testDir1);
+            Directory.CreateDirectory(testDir1 + "/dir");
+            File.CreateText(testDir1 + "/file").Close();
+
+            Directory.CreateDirectory(testDir2);
+            Directory.CreateDirectory(testDir2 + "/dir");
+            File.CreateText(testDir2 + "/file").Close();
+
+            var (_, hash1) = Hasher.EvaluateHash(testDir1);
+            var (_, hash2) = Hasher.EvaluateHash(testDir2);
+
+            Assert.AreEqual(hash1, hash2);
+        }
+
+        [Test]
+        public void HasherShouldGetNotEqualHash()
+        {
+            var testDir1 = "TestDir/HasherShouldGetDifferentHash1";
+            var testDir2 = "TestDir/HasherShouldGetDifferentHash2";
+
+            Directory.CreateDirectory(testDir1);
+            Directory.CreateDirectory(testDir1 + "/dir");
+            File.CreateText(testDir1 + "/file").Close();
+
+            Directory.CreateDirectory(testDir2);
+            Directory.CreateDirectory(testDir2 + "/dir");
+            File.CreateText(testDir2 + "/otherFile").Close();
+
+            var (_, hash1) = Hasher.EvaluateHash(testDir1);
+            var (_, hash2) = Hasher.EvaluateHash(testDir2);
+
+            Assert.AreNotEqual(hash1, hash2);
+        }
     }
 }
