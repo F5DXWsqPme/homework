2381364e31dba4d1cb9856d34792b63b1fcf7724
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2021-02-16 21:03:01 +0300

    solution

---
+++ HW4wt_1_2/Solution/Program.fs
@@ -0,0 +1,13 @@
+﻿let rec revertInner list result =
+    match list with
+    | [] -> result
+    | head::tail -> revertInner tail (head :: result)
+
+let rec revert list =
+    revertInner list []
+
+[<EntryPoint>]
+let main argv =
+    let list = [1; 2; 3]
+    printfn "%A" (revert list)
+    0
