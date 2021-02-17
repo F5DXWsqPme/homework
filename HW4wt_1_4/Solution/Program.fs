f3b1177e596d6863e20f71b81711bd89c8a3e066
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2021-02-17 12:49:45 +0300

    solution

---
+++ HW4wt_1_4/Solution/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let rec factorialInner acc n =
+    if n = 0 then
+        acc
+    else
+        factorialInner (acc * n) (n - 1)
+
+let factorial n =
+    if n < 0 then
+        None
+    else
+        Some(factorialInner 1 n)
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (factorial 4)
+    0
