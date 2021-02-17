7643f3629021012fa398a52a1302319e41782031
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2021-02-17 13:03:12 +0300

    solution

---
+++ HW4wt_1_5/Solution/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+let rec fibonacciInner fibonacci0 fibonacci1 n =
+    if n = 0 then
+        fibonacci0
+    else
+        let newFibonacci = fibonacci0 + fibonacci1
+        fibonacciInner fibonacci1 newFibonacci (n - 1)
+
+let fibonacci n =
+    if n < 0 then
+        None
+    else
+        Some(fibonacciInner 0 1 n)
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (fibonacci 3)
+    0
